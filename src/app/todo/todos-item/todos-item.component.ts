3e51b703eb90cef43ed532dd815a39a90bec931d
druiz5 <d.ruiz@t-systems.com>
2018-09-10 10:03:14 +0200

    Inittial commit

---
+++ src/app/todo/todos-item/todos-item.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../model/todo.model';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import { ToggleTodoAction, EditarTodoAction, Acciones, BorrarTodoAction } from '../todo.actions';
+
+@Component({
+  selector: 'app-todos-item',
+  templateUrl: './todos-item.component.html',
+  styles: []
+})
+export class TodosItemComponent implements OnInit {
+
+  @Input() todo: Todo;
+  @ViewChild('txtInputText') txtInputText: ElementRef;
+
+  chkField: FormControl;
+  txtInput: FormControl;
+
+  editando: boolean;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.chkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+    this.chkField.valueChanges.subscribe( () => {
+      const accion = new ToggleTodoAction( this.todo.id);
+      this.store.dispatch(accion);
+    });
+  }
+
+  editar() {
+    this.editando = true;
+    setTimeout( () => {
+      this.txtInputText.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+
+    if (this.txtInput.invalid ||
+        this.txtInput.value === this.todo.texto) {
+      return;
+    }
+
+    const accion = new EditarTodoAction (this.todo.id, this.txtInput.value);
+    this.store.dispatch(accion);
+  }
+
+  borrarTodo() {
+
+    const acction = new BorrarTodoAction(this.todo.id);
+    this.store.dispatch(acction);
+  }
+
+}
